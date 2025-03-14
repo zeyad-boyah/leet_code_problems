8ad08467d77d7a7545c66f605b3836b8012d5cba
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-14 04:33:21 +0200

    add linked_list_cycle sol

---
+++ linked_list_cycle/linked_list_cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head == None:
+            return False
+        iterator1 = head
+        iterator2 = head
+        while iterator2 and iterator2.next:
+            iterator1 = iterator1.next # normal speed iterator
+            iterator2 = iterator2.next.next # double speed iterator
+            if iterator1 == iterator2:
+                return True
+        return False
+
+        
