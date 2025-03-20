e5735ff2fa5f0b94f1fa468b8db4e5b0a2992a0b
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-03-20 02:21:40 +0200

    add better sol for valid anagram

---
+++ arrays_and_hashing/valid_anagram/valid_anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hashed_s = [0] * 26 # from a-z
+        hashed_t = [0] * 26
+        for L in range(len(s)):
+            hashed_s[ord(s[L])-ord('a')] +=1
+            hashed_t[ord(t[L])-ord('a')] +=1
+
+            
+        return hashed_s == hashed_t
