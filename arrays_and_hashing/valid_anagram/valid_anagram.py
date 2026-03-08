da7a56cf03ada63c46db60445a1524c86949c262
boyah@archlinux <zeyadelgohary.ze@gmail.com>
2026-03-08 12:38:54 +0200

    valid anagram (duplicate)

--- arrays_and_hashing/valid_anagram/valid_anagram.py
+++ arrays_and_hashing/valid_anagram/valid_anagram.py
@@ -1,4 +1,21 @@
-class Solution:
+class SolutionOne:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # make a dict with the first string that contains every letter and it's number of occurance 
+        # then iterate over the other string and deduct the number of letters if they exist in the first string 
+        dict_of_s = {}
+        if len(s) != len(t): return False
+        for letter in s:
+            if letter in dict_of_s.keys():
+                dict_of_s[letter] = dict_of_s[letter] + 1
+            else: dict_of_s[letter] = 1
+        for letter in t:
+            if letter in dict_of_s.keys() and dict_of_s[letter] != 0 :
+                dict_of_s[letter] = dict_of_s[letter] - 1
+            else: return False
+
+        return True
+
+class SolutionTwo:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
