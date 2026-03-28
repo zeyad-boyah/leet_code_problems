2d02f6c67fec6fa59970e091b6724e16d2507767
zeyad@desktop <zeyadelgohary.ze@gmail.com>
2026-03-28 23:40:09 +0200

    daily temp remake

--- stacks/daily_temp.py
+++ stacks/daily_temp.py
@@ -2,21 +2,16 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # the problem:
-        # i need to make an array that tells when will a higher
-        # temp occur from the curret iterable without being O(n^2)
-        # suggested solution is to use a monotonic stack
-        # by building the stack so that it keeps Desc order we can keep track of the day
-        # by simply poping the elements when we reach a higher temp and 
-        # calculating the difference between it's index and the current element
-        res = [0]* len(temperatures)
-        mono_stack = []
-        for i,t in enumerate(temperatures):
-            while mono_stack and t > mono_stack[-1][0]:
-                head_temp, head_index = mono_stack.pop()
-                res[head_index] = i - head_index
-            mono_stack.append((t,i))
-        return res
+        # use a monotonic stack where we maintain descending order
+        stack = []
+        ans =[0]* len(temperatures)
+        for i,temp in enumerate(temperatures):
+            while stack and stack[-1][1] < temp:
+                day= stack.pop()
+                ans[day[0]]= i - day[0]
+            stack.append((i,temp))
+        return ans
+
     
 
 test = Solution()
