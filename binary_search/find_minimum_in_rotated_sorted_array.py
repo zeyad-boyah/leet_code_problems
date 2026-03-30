b15f7d95188ce17ce17ac5e98b664f21efdc67c1
boyah@archlinux <zeyadelgohary.ze@gmail.com>
2026-03-30 12:34:36 +0200

    Find Minimum in Rotated Sorted Array (improved)

--- binary_search/find_minimum_in_rotated_sorted_array.py
+++ binary_search/find_minimum_in_rotated_sorted_array.py
@@ -8,16 +8,15 @@ class Solution:
         l,r = 0, len(nums)-1
         # in case of zero rotations
         if nums[l] < nums[r]:
-                return nums[l]
-        ans = nums[r]
-        while l <= r:
+            return nums[l]
+        while l < r:
             m= (l+r)//2
-            ans = min(ans,nums[m])
-            if nums[m] < nums[r]:
-                r = m-1
-            elif nums[l] <= nums[m]:
-                l = m+1
-        return ans
+            if nums[m] > nums[r]:
+                l= m +1
+            else:
+                r = m
+        return nums[l]
+
     
 test = Solution()
 nums=[4,5,6,7,0,1,2]
